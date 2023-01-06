c33cdbaec81bc1f489c80f9b4def1406459fe1a4
Ian Senne <ianssenne@gmail.com>
2023-01-06 11:16:24 -0800

    Revert "added Sortable interaction"
    This reverts commit 2953cbb39fc82f6fdc920e04ced45798736cd98f.

--- script.js
+++ script.js
@@ -55,17 +55,3 @@ $(function () {
 		source: skillNames,
 	});
 });
-// Datepicker widget
-$(function () {
-	$("#datepicker").datepicker({
-		changeMonth: true,
-		changeYear: true,
-	});
-});
-
-// Sortable interaction
-$(function () {
-	$("#skills-list").sortable({
-		placeholder: "ui-state-highlight",
-	});
-});
